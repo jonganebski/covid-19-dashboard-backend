235b8c90dc201c5b29a3274f695aac120de5af36
Jon Ganebski <jon.ganebski@gmail.com>
2020-11-24 09:31:46 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,16 @@ const PORT = process.env.PORT || 4000;
 const CORS_ORIGIN =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://see-covid19.netlify.app/";
+    : "https://see-covid19.netlify.app";
 
 const app = express();
 
 app.use(helmet());
 
+app.use(cors({ origin: CORS_ORIGIN }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors({ origin: CORS_ORIGIN }));
 
 app.post("/api/news", async (req, res) => {
   const { country } = req.body;
